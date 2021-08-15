85b67b35a98a19d987568bffb74ac4981a3191f3
Jason <jason.limon@outlook.com>
2021-08-15 14:51:40 -0500

    Mas de clases

--- 04-clases/Musico.py
+++ 04-clases/Musico.py
@@ -6,6 +6,10 @@ class Musico:
         self.nombre = nombre
         self.instrumento = instrumento
     
+    # Hasta los metodos deben llevar la referencia de self ya que es obligatorio
+    def mostrar_detalle(self):
+        print(f'El musico {self.nombre} toca el o la {self.instrumento}')
+
 # Como podemos ver al crear el objeto no usamos new como en otros lenguajes y ademas no pasamos el self al momento de instanciar el objeto
 guitarrista = Musico('Jason', 'Guitarra')
 
@@ -15,3 +19,17 @@ print(guitarrista.instrumento)
         
 bajista = Musico('Mauricio', 'Bajo')
 print(f'{bajista.nombre} toca el {bajista.instrumento}')
+
+bajista.mostrar_detalle()
+
+# Podemos mandar a llamar al metodo con la clase pero necesitamos mandar por parametro la referencia de un objeto
+Musico.mostrar_detalle(guitarrista)
+
+
+# Podemos agregar un nuevo atributo a la clase pero solo indicado a un sol oobjeto por ejemplo
+guitarrista.concierto = 'Auditorio Nacional'
+
+print(guitarrista.concierto)
+print(bajista.concierto)
+
+# Mandara un error  en el caso de nuestro primer objeto si nos tomara el atributo como existente
